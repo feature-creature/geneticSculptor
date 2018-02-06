8ded15a538ddd0f33b0638d0a48e4415ddcde404
Feature Creature <jdema003@gold.ac.uk>
2018-02-06 18:12:32 +0000

    manipulation of branch proportion genes

--- Tree.cpp
+++ Tree.cpp
@@ -24,22 +24,21 @@ void Tree::setup(int _numOfGenes)
 void Tree::calcPhenotype(){
         //decode the genes into variables for drawing
         branchOffset = ofMap(dna.genes[0],0,1,0,1);
-        startTheta = ofMap(dna.genes[1],0,1, 0, 2);
-        startLength = ofMap(dna.genes[2],0,1, 50, 120);
+        startTheta = ofMap(dna.genes[1],0,1, 0, 30);
+        startLength = ofMap(dna.genes[2],0,1, 20, 100);
         minLength = ofMap(dna.genes[3],0,1, 3, 10);
         branchReduce = ofMap(dna.genes[4],0,1, 0.45, 0.7);
-//        thetaVariance = ofMap(dna.genes[5],0,1, 0, 30);
-        thetaVariance = dna.genes[5];
+        thetaVariance = ofMap(dna.genes[5],0,1, 0, 30);
         reduceVariance = ofMap(dna.genes[6],0,1, 0, 0.1);
         startAngle = ofMap(dna.genes[7],0,1, 0, 30);
         startWidth = ofMap(dna.genes[8],0,1, 2, 20);
-        erosionFactor = ofMap(dna.genes[9],0,1, 0.2, 0.8);
+        erosionFactor = ofMap(dna.genes[9],0,1, 0.3, 0.5);
         leafSize = ofMap(dna.genes[10],0,1,1,6);
         leafColorR = ofMap(dna.genes[11],0,1,0,255);
         leafColorG = ofMap(dna.genes[12],0,1,0,255);
         leafColorB = ofMap(dna.genes[13],0,1,0,255);
-        leafColorA = ofMap(dna.genes[14],0,1,0,25);
-        leafShape = ofMap(dna.genes[15],0,1,1,5);
+        leafColorA = ofMap(dna.genes[14],0,1,25,100);
+        leafShape = ofMap(dna.genes[15],0,1,3,5);
         seed = ofRandom(1000,65000);
 }
 void Tree::branch(float b_length, float theta, float b_width)
@@ -57,13 +56,13 @@ void Tree::branch(float b_length, float theta, float b_width)
         
         ofPushMatrix();
         ofTranslate(0,-b_length);
-        ofRotate(theta + (ofNoise(thetaVariance, ofGetFrameNum()/60.0)*100));
+        ofRotate(theta + (ofNoise(ofGetFrameNum()/(10*b_length))));
         branch(b_length * (branchReduce + ofRandom(-reduceVariance, reduceVariance)), theta, b_width * erosionFactor);
         ofPopMatrix();
         
         ofPushMatrix();
-        ofTranslate(0,-b_length * branchOffset);
-        ofRotate(-theta - (ofNoise(thetaVariance, ofGetFrameNum()/60.0)*100));
+        ofTranslate(0,-b_length * 1.5);
+        ofRotate(-theta - (ofNoise(ofGetFrameNum()/thetaVariance, ofGetFrameNum()/b_length)));
         branch(b_length * (branchReduce + ofRandom(-reduceVariance, reduceVariance)), theta, b_width * erosionFactor);
         ofPopMatrix();
     }
@@ -72,6 +71,7 @@ void Tree::branch(float b_length, float theta, float b_width)
         for(int i = 0; i < 20; i++)
         {
             ofSetColor(leafColorR,leafColorG,leafColorB,leafColorA);
+            //ofSetColor(leafColorB,leafColorA);
             ofDrawEllipse(ofRandom(-4,4),ofRandom(-4,4),leafSize,leafSize);
         }
     }
